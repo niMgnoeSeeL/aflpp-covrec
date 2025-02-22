308e2f41ed51bc8d7eb196b75ca90ec247d696cb
Seongmin Lee <nim.gnoes.eel@gmail.com>
2025-02-22 15:07:37 +0000

    Add reset estimator

--- custom_mutators/examples/custom_post_run.c
+++ custom_mutators/examples/custom_post_run.c
@@ -22,9 +22,15 @@ typedef struct record {
   // stats
   long long unsigned int time_ms;
   long long unsigned int execs;
-  long unsigned int      n_covered;
-  long unsigned int      n_sglt_clusts;
-  long unsigned int      n_singletons;
+  u32                    n_seeds;
+  // total
+  long unsigned int      n_covered_total;
+  long unsigned int      n_sglt_clusts_total;
+  long unsigned int      n_singletons_total;
+  // reset
+  long unsigned int      n_covered_reset;
+  long unsigned int      n_sglt_clusts_reset;
+  long unsigned int      n_singletons_reset;
 
   // for ground truth computation
   SimpleSet             *covered;
@@ -43,18 +49,24 @@ typedef struct setofset {
   struct setofset *next;
 } setofset_t;
 
-typedef struct my_mutator {
-  afl_state_t *afl;
-
+typedef struct covmanager {
   u32         n_execs;
   SimpleSet  *covered_prev;
   setofset_t *sglt_clusts;
   u32         n_sglt_clusts;
   SimpleSet  *singletons;
-  u64         last_record_add_time;
+} covmanager_t;
+
+typedef struct my_mutator {
+  afl_state_t *afl;
+
+  covmanager_t *covman_total;
+  covmanager_t *covman_reset;
+  u32           n_prev_seeds;
 
   record_t *records;
   u32       records_len;
+  u64       last_record_add_time;
 
   bool force_save;
   bool reset_after_tmin;
@@ -72,6 +84,46 @@ inline u64 get_cur_time(void) {
   return (tv.tv_sec * 1000ULL) + (tv.tv_usec / 1000);
 }
 
+covmanager_t *covmanager_init(void) {
+  covmanager_t *covman = (covmanager_t *)malloc(sizeof(covmanager_t));
+  covman->n_execs = 0;
+  covman->covered_prev = (SimpleSet *)malloc(sizeof(SimpleSet));
+  set_init(covman->covered_prev);
+  covman->sglt_clusts = NULL;
+  covman->n_sglt_clusts = 0;
+  covman->singletons = (SimpleSet *)malloc(sizeof(SimpleSet));
+  set_init(covman->singletons);
+  return covman;
+}
+
+void reset_covmanager(covmanager_t *covman) {
+  // Note. we do not reset n_execs
+  set_clear(covman->covered_prev);
+  setofset_t *cur = covman->sglt_clusts;
+  while (cur) {
+    set_destroy(cur->set);
+    setofset_t *tmp = cur;
+    cur = cur->next;
+    free(tmp);
+  }
+  covman->sglt_clusts = NULL;
+  covman->n_sglt_clusts = 0;
+  set_clear(covman->singletons);
+}
+
+void destroy_covmanager(covmanager_t *covman) {
+  set_destroy(covman->covered_prev);
+  setofset_t *cur = covman->sglt_clusts;
+  while (cur) {
+    set_destroy(cur->set);
+    setofset_t *tmp = cur;
+    cur = cur->next;
+    free(tmp);
+  }
+  set_destroy(covman->singletons);
+  free(covman);
+}
+
 my_mutator_t *afl_custom_init(afl_state_t *afl, unsigned int seed) {
   my_mutator_t *data = calloc(1, sizeof(my_mutator_t));
   if (!data) {
@@ -80,15 +132,11 @@ my_mutator_t *afl_custom_init(afl_state_t *afl, unsigned int seed) {
   }
 
   data->afl = afl;
-  data->n_execs = 0;
-  data->covered_prev = (SimpleSet *)malloc(sizeof(SimpleSet));
-  set_init(data->covered_prev);
-  data->sglt_clusts = NULL;
-  data->n_sglt_clusts = 0;
-  data->singletons = (SimpleSet *)malloc(sizeof(SimpleSet));
-  set_init(data->singletons);
-  // data->trace_bits_prev = ck_alloc(afl->fsrv.map_size);
-  // memset(data->trace_bits_prev, 0, afl->fsrv.map_size);
+
+  data->covman_total = covmanager_init();
+  data->covman_reset = covmanager_init();
+  data->n_prev_seeds = afl->queued_items;
+  
   data->records = NULL;
   data->records_len = 0;
   data->force_save = false;
@@ -110,26 +158,12 @@ my_mutator_t *afl_custom_init(afl_state_t *afl, unsigned int seed) {
   return data;
 }
 
-void reset_data(my_mutator_t *data) {
-  data->n_execs = 0;
-  set_clear(data->covered_prev);
-  setofset_t *cur = data->sglt_clusts;
-  while (cur) {
-    set_destroy(cur->set);
-    setofset_t *tmp = cur;
-    cur = cur->next;
-    free(tmp);
-  }
-  data->sglt_clusts = NULL;
-  data->n_sglt_clusts = 0;
-  set_clear(data->singletons);
-  record_t *cur_record = data->records;
-  while (cur_record) {
-    set_destroy(cur_record->covered);
-    record_t *tmp = cur_record;
-    cur_record = cur_record->next;
-    free(tmp);
-  }
+void reset_entire_data(my_mutator_t *data) {
+  destroy_covmanager(data->covman_total);
+  destroy_covmanager(data->covman_reset);
+  data->covman_total = covmanager_init();
+  data->covman_reset = covmanager_init();
+
   data->records = NULL;
   data->records_len = 0;
   data->force_save = false;
@@ -143,29 +177,104 @@ const char *idx_to_str(u32 idx) {
   return buf;
 }
 
+bool update_covmanager(
+  covmanager_t *covman, char *key, SimpleSet *new_sglt_clust
+) {
+  bool add_new_record = false;
+  if (set_contains(covman->covered_prev, key) == SET_FALSE) {
+    add_new_record = true;
+    set_add(covman->covered_prev, key);
+    set_add(covman->singletons, key);
+    set_add(new_sglt_clust, key);
+  } else {
+    // if the key was in singletons, remove it from singletons and
+    // sglt_clusts
+    if (set_contains(covman->singletons, key) == SET_TRUE) {
+      add_new_record = true;
+      set_remove(covman->singletons, key);
+      // iterate over the sglt_clusts and remove the key from the sets
+      setofset_t *cur = covman->sglt_clusts;
+      while (cur) {
+        if (set_contains(cur->set, key) == SET_TRUE) {
+          set_remove(cur->set, key);
+          if (set_length(cur->set) == 0) {
+            setofset_t *tmp = cur;
+            cur = cur->next;
+            if (tmp == covman->sglt_clusts) {
+              covman->sglt_clusts = cur;
+            } else {
+              setofset_t *prev = covman->sglt_clusts;
+              while (prev->next != tmp) {
+                prev = prev->next;
+              }
+              prev->next = cur;
+            }
+            set_destroy(tmp->set);
+            free(tmp);
+            covman->n_sglt_clusts--;
+          }
+          break;
+        } else {
+          cur = cur->next;
+          // if cur is NULL, something is wrong
+          if (!cur) {
+            FATAL("Error: key %s is in singletons but not in sglt_clusts",
+                  key);
+          }
+        }
+      }
+    }
+  }
+  return add_new_record;
+}
+
+void update_singleton_clusters(covmanager_t *covman, SimpleSet *new_sglt_clust) {
+  // if there is new singleton cluster, add it to sglt_clusts
+  if (set_length(new_sglt_clust) > 0) {
+    setofset_t *new_sglt_clust_node = (setofset_t *)malloc(sizeof(setofset_t));
+    new_sglt_clust_node->set = new_sglt_clust;
+    new_sglt_clust_node->next = covman->sglt_clusts;
+    covman->sglt_clusts = new_sglt_clust_node;
+    covman->n_sglt_clusts++;
+  } else {
+    set_destroy(new_sglt_clust);
+    free(new_sglt_clust);
+  }
+}
+
 void afl_custom_post_run(my_mutator_t *data) {
   // printf("|C%d", data->afl->record_sampling);
   if (data->reset_after_tmin &&
       get_cur_time() - data->afl->start_time > data->tmin) {
-    reset_data(data);
+    reset_entire_data(data);
     data->reset_after_tmin = false;
   }
 
   if (!data->afl->record_sampling) { return; }
   // printf("|R%d", data->afl->record_sampling);
   data->afl->record_sampling = false;
-  data->n_execs++;
+  data->covman_total->n_execs++;
+  data->covman_reset->n_execs = data->covman_total->n_execs;
 
   u32  i;
   bool add_new_record = false;
 
-  SimpleSet *new_sglt_clust = (SimpleSet *)malloc(sizeof(SimpleSet));
-  set_init(new_sglt_clust);
+  // check whether the number of seeds has changed
+  if (data->n_prev_seeds != data->afl->queued_items) {
+    data->n_prev_seeds = data->afl->queued_items;
+    add_new_record = true;
+    reset_covmanager(data->covman_reset);
+  }
+
+  SimpleSet *new_sglt_clust_total = (SimpleSet *)malloc(sizeof(SimpleSet));
+  set_init(new_sglt_clust_total);
+  SimpleSet *new_sglt_clust_reset = (SimpleSet *)malloc(sizeof(SimpleSet));
+  set_init(new_sglt_clust_reset);
 
   // flag up the check_new for all records. this recording for the missing mass
   // analysis only done until the number of executions is doubled.
   record_t *cur = data->records;
-  while (cur && cur->execs * 2 >= data->n_execs) {
+  while (cur && cur->execs * 2 >= data->covman_total->n_execs) {
     cur->check_new = true;
     cur = cur->prev;
   }
@@ -186,64 +295,20 @@ void afl_custom_post_run(my_mutator_t *data) {
         }
         cur = cur->prev;
       }
-      // if the key is not in covered_prev, add it to singletons and sglt_clusts
-      if (set_contains(data->covered_prev, key) == SET_FALSE) {
-        add_new_record = true;
-        set_add(data->covered_prev, key);
-        set_add(data->singletons, key);
-        set_add(new_sglt_clust, key);
-      } else {
-        // if the key was in singletons, remove it from singletons and
-        // sglt_clusts
-        if (set_contains(data->singletons, key) == SET_TRUE) {
-          add_new_record = true;
-          set_remove(data->singletons, key);
-          // iterate over the sglt_clusts and remove the key from the sets
-          setofset_t *cur = data->sglt_clusts;
-          while (cur) {
-            if (set_contains(cur->set, key) == SET_TRUE) {
-              set_remove(cur->set, key);
-              if (set_length(cur->set) == 0) {
-                setofset_t *tmp = cur;
-                cur = cur->next;
-                if (tmp == data->sglt_clusts) {
-                  data->sglt_clusts = cur;
-                } else {
-                  setofset_t *prev = data->sglt_clusts;
-                  while (prev->next != tmp) {
-                    prev = prev->next;
-                  }
-                  prev->next = cur;
-                }
-                set_destroy(tmp->set);
-                free(tmp);
-                data->n_sglt_clusts--;
-              }
-              break;
-            } else {
-              cur = cur->next;
-              // if cur is NULL, something is wrong
-              if (!cur) {
-                FATAL("Error: key %s is in singletons but not in sglt_clusts",
-                      key);
-              }
-            }
-          }
-        }
-      }
+
+      // update covmanager:
+      // if the key was not in covered_prev, add it as a new singleton
+      // if the key was in singletons, remove it from singletons
+      add_new_record = update_covmanager(
+        data->covman_total, key, new_sglt_clust_total) || add_new_record;
+      add_new_record = update_covmanager(
+        data->covman_reset, key, new_sglt_clust_reset) || add_new_record;
     }
   }
   // if there is new singleton cluster, add it to sglt_clusts
-  if (set_length(new_sglt_clust) > 0) {
-    setofset_t *new_sglt_clust_node = (setofset_t *)malloc(sizeof(setofset_t));
-    new_sglt_clust_node->set = new_sglt_clust;
-    new_sglt_clust_node->next = data->sglt_clusts;
-    data->sglt_clusts = new_sglt_clust_node;
-    data->n_sglt_clusts++;
-  } else {
-    set_destroy(new_sglt_clust);
-    free(new_sglt_clust);
-  }
+  update_singleton_clusters(data->covman_total, new_sglt_clust_total);
+  update_singleton_clusters(data->covman_reset, new_sglt_clust_reset);
+
   // if the singleton status has changed, add a new record
   // otherwise, if it has been 10 minutes since the last record or the
   // force_save is true, add a new record
@@ -266,18 +331,26 @@ void afl_custom_post_run(my_mutator_t *data) {
     record_t *new_record = (record_t *)malloc(sizeof(record_t));
     new_record->time_ms = get_cur_time() - data->afl->start_time;
     if (!data->reset_after_tmin) { new_record->time_ms -= data->tmin; }
-    new_record->execs = data->n_execs;
-    new_record->n_covered = set_length(data->covered_prev);
-    new_record->n_sglt_clusts = data->n_sglt_clusts;
-    new_record->n_singletons = set_length(data->singletons);
+    new_record->execs = data->covman_total->n_execs;
+    new_record->n_seeds = data->afl->queued_items;
+    
+    new_record->n_covered_total = set_length(data->covman_total->covered_prev);
+    new_record->n_sglt_clusts_total = data->covman_total->n_sglt_clusts;
+    new_record->n_singletons_total = set_length(data->covman_total->singletons);
+    
+    new_record->n_covered_reset = set_length(data->covman_reset->covered_prev);
+    new_record->n_sglt_clusts_reset = data->covman_reset->n_sglt_clusts;
+    new_record->n_singletons_reset = set_length(data->covman_reset->singletons);
+    
     SimpleSet *covered_so_far = (SimpleSet *)malloc(sizeof(SimpleSet));
     set_init(covered_so_far);
-    for (uint64_t i = 0; i < data->covered_prev->number_nodes; ++i) {
-      if (data->covered_prev->nodes[i] != NULL) {
-        set_add(covered_so_far, data->covered_prev->nodes[i]->_key);
+    for (uint64_t i = 0; i < data->covman_total->covered_prev->number_nodes; ++i) {
+      if (data->covman_total->covered_prev->nodes[i] != NULL) {
+        set_add(covered_so_far, data->covman_total->covered_prev->nodes[i]->_key);
       }
     }
     new_record->covered = covered_so_far;
+    
     new_record->n_found_new = 0;
     new_record->is_update = add_new_record;
     new_record->prev = data->records;
@@ -322,17 +395,24 @@ void update_record(my_mutator_t *data) {
     return;
   }
   fprintf(f,
-          "time, #execs, #covered, #singletons, #sglt_clusts, #foundnew, done, "
-          "update?\n");
+          "time, #execs, #seeds, "
+          "#covered, #singletons, #sglt_clusts, "
+          "#coveredR, #singletonsR, #sglt_clustsR, "
+          "#foundnew, done, update?\n");
   record_t *cur = data->records;
   // find the first record
   while (cur && cur->prev) {
     cur = cur->prev;
   }
   while (cur) {
-    fprintf(f, "%llu, %llu, %lu, %lu, %lu, %llu, %s, %s\n", cur->time_ms,
-            cur->execs, cur->n_covered, cur->n_singletons, cur->n_sglt_clusts,
-            cur->n_found_new, cur->execs * 2 < data->n_execs ? "true" : "false",
+    fprintf(f, "%llu, %llu, %u, "
+            "%lu, %lu, %lu, "
+            "%lu, %lu, %lu, "
+            "%llu, %s, %s\n", 
+            cur->time_ms, cur->execs, cur->n_seeds,
+            cur->n_covered_total, cur->n_singletons_total, cur->n_sglt_clusts_total,
+            cur->n_covered_reset, cur->n_singletons_reset, cur->n_sglt_clusts_reset,
+            cur->n_found_new, cur->execs * 2 < data->covman_total->n_execs ? "true" : "false",
             cur->is_update ? "true" : "false");
     cur = cur->next;
   }
@@ -353,9 +433,6 @@ void afl_custom_end_job(my_mutator_t *data) {
 }
 
 void afl_custom_deinit(my_mutator_t *data) {
-  set_destroy(data->covered_prev);
-  set_destroy(data->singletons);
-  // ck_free(data->trace_bits_prev);
   record_t *cur = data->records;
   while (cur) {
     record_t *tmp = cur;
@@ -363,13 +440,10 @@ void afl_custom_deinit(my_mutator_t *data) {
     set_destroy(tmp->covered);
     free(tmp);
   }
-  setofset_t *cur_set = data->sglt_clusts;
-  while (cur_set) {
-    set_destroy(cur_set->set);
-    setofset_t *tmp = cur_set;
-    cur_set = cur_set->next;
-    free(tmp);
-  }
+  destroy_covmanager(data->covman_total);
+  destroy_covmanager(data->covman_reset);
+  free(data->covman_total);
+  free(data->covman_reset);
 
   free(data);
 }
